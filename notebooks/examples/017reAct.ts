115ba23fd7997fad262c73c42242d40a6a730ff3
mudssky <mudssky@gmail.com>
2025-03-12 15:20:59 +0800

    ✨ feat: add reAct agent notes

---
+++ notebooks/examples/017reAct.ts
@@ -0,0 +1,31 @@
+import { Calculator } from "@langchain/community/tools/calculator";
+import { TavilySearchResults } from "@langchain/community/tools/tavily_search";
+import { pull } from "langchain/hub";
+import { PromptTemplate } from "@langchain/core/prompts";
+import { ChatDeepSeek } from "@langchain/deepseek";
+import { createReactAgent, AgentExecutor } from "langchain/agents";
+import * as dotenv from "dotenv";
+async function run() {
+  const tools = [new TavilySearchResults(), new Calculator()];
+  const prompt = await pull<PromptTemplate>("hwchase17/react");
+  const llm = new ChatDeepSeek({
+    model: "deepseek-chat",
+    temperature: 0,
+  });
+  const agent = await createReactAgent({
+    llm,
+    tools,
+    prompt,
+  });
+  const agentExecutor = new AgentExecutor({
+    agent,
+    tools,
+  });
+  const result = await agentExecutor.invoke({
+    input: "我有 17 美元，现在相当于多少人民币？",
+  });
+  console.log({ result });
+}
+
+dotenv.config();
+run();
