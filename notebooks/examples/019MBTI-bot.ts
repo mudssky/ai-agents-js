f3f58706a909478695c23328bb22ad71f7c008c0
mudssky <mudssky@gmail.com>
2025-03-13 10:48:31 +0800

    ✨ feat: mbti bot

--- notebooks/examples/019MBTI-bot.ts
+++ notebooks/examples/019MBTI-bot.ts
@@ -1,9 +1,23 @@
 import { StringOutputParser } from "@langchain/core/output_parsers";
-import { ChatPromptTemplate, PromptTemplate } from "@langchain/core/prompts";
-import { RunnableSequence } from "@langchain/core/runnables";
+import {
+  ChatPromptTemplate,
+  MessagesPlaceholder,
+  PromptTemplate,
+} from "@langchain/core/prompts";
+import {
+  RunnableSequence,
+  RunnableWithMessageHistory,
+} from "@langchain/core/runnables";
+import { DynamicStructuredTool } from "@langchain/core/tools";
 import { ChatDeepSeek } from "@langchain/deepseek";
+import { z } from "zod";
+import mbtiInfo from "../data/mbtiInfo.json";
+import readline from "readline";
+import { ChatMessageHistory } from "langchain/memory";
+import { AgentExecutor, createOpenAIToolsAgent } from "langchain/agents";
+import * as dotenv from "dotenv";
 
-async function run() {
+async function getAgent() {
   const prompt = ChatPromptTemplate.fromMessages([
     [
       "system",
@@ -23,4 +37,96 @@ async function run() {
     model,
     new StringOutputParser(),
   ]);
+  const mbtiList: any = Object.keys(mbtiInfo);
+  const mbtiTool = new DynamicStructuredTool({
+    name: "get-mbti-chat",
+    schema: z.object({
+      type: z.enum(mbtiList).describe("用户的 MBTI 类型"),
+      question: z.string().describe("用户的问题"),
+    }),
+    func: async ({ type, question }) => {
+      const info = mbtiInfo[type as keyof typeof mbtiInfo];
+      const res = await mbtiChain.invoke({ type, question, info });
+      return res;
+    },
+    description: "根据用户的问题和 MBTI 类型，回答用户的问题",
+  });
+
+  const tools = [mbtiTool];
+
+  const agentPrompt = await ChatPromptTemplate.fromMessages([
+    [
+      "system",
+      "你是一个用户接待的 agent，通过自然语言询问用户的 MBTI 类型和问题，直到你有足够的信息调用 get-mbti-chat 来回答用户的问题",
+    ],
+    new MessagesPlaceholder("history_message"),
+    ["human", "{input}"],
+    new MessagesPlaceholder("agent_scratchpad"),
+  ]);
+
+  const llm = new ChatDeepSeek({
+    model: "deepseek-chat",
+    temperature: 0.4,
+  });
+  const agent = await createOpenAIToolsAgent({
+    llm,
+    tools,
+    prompt: agentPrompt,
+  });
+
+  const agentExecutor = new AgentExecutor({
+    agent,
+    tools,
+  });
+  const messageHistory = new ChatMessageHistory();
+  const agentWithChatHistory = new RunnableWithMessageHistory({
+    runnable: agentExecutor,
+    getMessageHistory: () => messageHistory,
+    inputMessagesKey: "input",
+    historyMessagesKey: "history_message",
+  });
+  return agentWithChatHistory;
 }
+async function run() {
+  const agentWithChatHistory = await getAgent();
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  function chat() {
+    rl.question("User: ", async (input) => {
+      if (input.toLowerCase() === "exit") {
+        rl.close();
+        return;
+      }
+
+      const response = await agentWithChatHistory.invoke(
+        {
+          input,
+        },
+        {
+          configurable: {
+            sessionId: "no-used",
+          },
+        }
+      );
+
+      console.log("Agent: ", response.output);
+      // 检查是否已经调用了 get-mbti-chat 工具
+      const toolCalled = response.intermediateSteps?.some(
+        (step: any) => step.action?.tool === "get-mbti-chat"
+      );
+
+      if (toolCalled) {
+        console.log("已完成 MBTI 分析，聊天结束。");
+        rl.close();
+        return;
+      }
+      chat();
+    });
+  }
+  chat();
+}
+dotenv.config();
+run();
